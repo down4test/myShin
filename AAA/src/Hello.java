3d8fc024b55a3946ae8c0748e89a873f5ffe6db3
sputnik5757 <sputnik5757@gmail.com>
2016-12-28 15:47:54 +0900

    changed

--- AAA/src/Hello.java
+++ AAA/src/Hello.java
@@ -2,18 +2,9 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("new Test.....");
 		System.out.println("MergeTest");
-		System.out.println("MergeTest");
-		System.out.println("Hello");
-		System.out.println("MergeTest");
-		System.out.println("MergeTest");
-		System.out.println("Hello");
-		System.out.println("MergeTest");
-		System.out.println("MergeTest");
-		System.out.println("Hello");
-		System.out.println("dfadsfd");
-		System.out.println("MerdasfdfdfadfasdfageTest");
+		
 
 	}
 
